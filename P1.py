611f4c2979768a1ea186ff578c348a87bc6fb8a5
JVuns <71363642+JVuns@users.noreply.github.com>
2020-10-12 13:43:53 +0800

    Create P1.py

---
+++ P1.py
@@ -0,0 +1,5 @@
+x = lambda a : 15 + a
+print("it is ", x(eval(input("Number you want to add with 15: "))))
+
+z = lambda x, y : x*y
+print("y * x = ", z(int(input("enter x value: ")), int(input("enter y value: "))))
